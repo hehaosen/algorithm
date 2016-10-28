fbee918ffdb3737eac29d7a000982154b89a7bd8
hehaosen <307052084@qq.com>
2016-10-28 15:20:13 +0800

    +Nim Game

---
+++ 292NimGame.js
@@ -0,0 +1,13 @@
+/** 292. Nim Game
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    if (n < 4) {
+        return true;
+    }
+    if ( n % 4 === 0) {
+        return false
+    }
+    return true;
+};
